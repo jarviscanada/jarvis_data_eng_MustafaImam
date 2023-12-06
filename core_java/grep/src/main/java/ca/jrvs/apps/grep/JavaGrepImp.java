08dbdfa5a9226a8337d7e4d1ea08bf5f2ff33f1b
msaimam98 <maimam98@hotmail.com>
2023-12-06 00:45:50 -0500

    completing design implementation

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -2,11 +2,25 @@ package ca.jrvs.app.grep;
 
 import java.io.*;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.PatternSyntaxException;
+
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.log4j.BasicConfigurator;
 
-public class JavaGrepImp extends JavaGrep {
+
+
+public class JavaGrepImp implements JavaGrep {
 
     final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
 
@@ -22,6 +36,7 @@ public class JavaGrepImp extends JavaGrep {
     public String getRootPath() { return rootPath; }
     public void setRootPath(String rootPath) { this.rootPath = rootPath; }
 
+
     public static void main(String[] args) {
 
         if (args.length != 3) {
@@ -32,7 +47,7 @@ public class JavaGrepImp extends JavaGrep {
         BasicConfigurator.configure();
 
         JavaGrepImp javaGrepImp = new JavaGrepImp();
-        javaGrepImp.getRegex(args[0]);
+        javaGrepImp.setRegex(args[0]);
         javaGrepImp.setRootPath(args[1]);
         javaGrepImp.setOutFile(args[2]);
 
@@ -40,8 +55,163 @@ public class JavaGrepImp extends JavaGrep {
             javaGrepImp.process();
         }
         catch (Exception ex) {
-            javaGrepImp.logger.error("Error: Unable to process", ex)
+            javaGrepImp.logger.error("Error: Unable to process", ex);
         }
     }
+
+    // all the implements methods
+
+    /**
+     * Top level search workflow
+     * @throws IOException
+     */
+    @Override
+    public void process() throws IOException {
+        // Implementation of process method
+        List<String> matchedLines = new ArrayList<>();
+        for (File file : listFiles(getRootPath())) {
+            for (String line : readLines(file)) {
+                if (containsPattern(line)) {
+                    matchedLines.add(line);
+                }
+            }
+        }
+        writeToFile(matchedLines);
+    }
+
+    /**
+     * Traverse a given directory and return all files
+     * @param rootDir input directory
+     * @return files under the rootDir
+     * return a list of all files in this directory
+     */
+    @Override
+    public List<File> listFiles(String rootDir) {
+        List<File> fileList = new ArrayList<>();
+        File directory = new File(rootDir);
+        // Check if rootDir is a directory
+        if (directory.isDirectory()) {
+
+            // Call the recursive function to add files to fileList
+            addFilesToList(directory, fileList);
+        } else {
+            throw new IllegalArgumentException("The provided path is not a directory: " + rootDir);
+        }
+        return fileList;
+
+    };
+
+    private void addFilesToList(File directory, List<File> fileList) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    fileList.add(file);
+                } else if (file.isDirectory()) {
+                    addFilesToList(file, fileList); // Recursive call
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * Read a file and return all the lines
+     * Explain FileReader, BufferedReader, and character encoding
+     * @param inputFile file to be read
+     * @return lines
+     * @throws IllegalArgumentException if a given inputFile is not a file
+     * I have to find a way to go through every line somehow
+     */
+    @Override
+    public List<String> readLines(File inputFile) {
+        if (!inputFile.isFile()) {
+            throw new IllegalArgumentException(inputFile + "is not a file");
+        }
+
+        // List to populate
+        List<String> lines = new ArrayList<>();
+        try ( BufferedReader reader = new BufferedReader(new FileReader(inputFile)) ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("File not found: " + inputFile, e);
+        }
+        catch ( IOException  e ) {
+            logger.error("An I/O exception occurred", e);
+        }
+        return lines;
+    };
+
+    /**
+     * check if a Line contains the regex pattern (passed by user)
+     * @param line input string
+     * @return true if there is a match
+     */
+    @Override
+    public boolean containsPattern(String line) {
+
+        try {
+            String patternString = this.getRegex();
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(line);
+            return matcher.find();
+        }
+        catch (PatternSyntaxException e) {
+            logger.error("This pattern is throwing an error");
+            return false;
+
+
+        }
+    };
+
+    /**
+     * Write Lines to a file
+     * Explore: FileOutputStream, OutputStreamWriter, and BufferedWriter
+     * @param lines matched Line
+     * @throws IOException if write failed
+     */
+    @Override
+    public void writeToFile(List<String> lines) throws IOException {
+
+        // get the file
+        File outputFile = new File(getOutFile()); // we dont use this.getOutFile since this is only used in the local scope if there is a name conflict
+
+        try (
+        // output system setup
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFile); // Here we are essentially naming a file
+        // to write into. This stream is bytes only, when you write to the file it must be using a stream that writes
+        // in bytes OR we wrap this with another stream that converts normal characters into bytes.
+        // When you create a new instance of FileOutputStream, you specify a File object or a file path. This sets up a
+        // stream through which data can be written to the file specified. The data written through a FileOutputStream
+        // is in the form of bytes.
+
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream); //  It takes a byte stream
+        // (like FileOutputStream) and converts the characters into bytes according to the specified charset encoding
+        // This is necessary because the data in files is ultimately stored as bytes, but you often work with characters
+        // in a higher-level programming language like Java.
+
+        BufferedWriter writer = new BufferedWriter(outputStreamWriter); // It holds the characters in a buffer. When the
+        // buffer is full, or when the flush() or close() method is called, it writes the data to the underlying output
+        // stream (in this case, the OutputStreamWriter). This reduces the number of I/O operations by grouping
+        // characters together, which is more efficient than writing each character individually to the disk.
+
+        ) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+
+            }
+        } catch (IOException e) {
+            logger.error("There is an error writing to file: " + outputFile);
+
+        }
+    };
+
+
+
 
 }
--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -27,30 +27,3 @@ public interface RegexExc {
      */
     public boolean isEmptyLine(String line);
 }
-
-public class RegexExcImp implements RegexExc {
-
-    @java.lang.Override
-    public boolean matchJpeg(String filename) {
-
-        Pattern pattern = Pattern.compile(".+\\.(jpe?g)", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(filename);
-        return matcher.matches();
-    }
-
-    @java.lang.Override
-    public boolean matchIp(String ip) {
-        Pattern pattern = Pattern.compile("^([0-9]{1,3}\\.){3}[0-9]{1,3}$");
-        Matcher matcher = pattern.matcher(ip);
-        return matcher.matches();
-        // for either number to be greater than 999 it must be 4 digits, regex takes care of that
-    }
-
-    @java.lang.Override
-    public boolean isEmptyLine(String line) {
-        if (line == null || line.trim().isEmpty() ) {
-            return true
-        }
-        return false
-    }
-}
